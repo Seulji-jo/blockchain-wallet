8b99e2fdde171a6dad9be8b3dc990fa7c16b7c34
Seulji-jo <joseuling@gmail.com>
2022-09-21 16:41:34 +0900

    [edit] MetaMaskErc20 파일 정리

--- src/components/MetaMaskErc20.jsx
+++ src/components/MetaMaskErc20.jsx
@@ -1,76 +1,22 @@
-import { BigNumber, ethers } from 'ethers';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import useAddressInput from '../hooks/useAddressInput';
-import useCoinInput from '../hooks/useCoinInput';
-import useNetworks from '../hooks/useNetworks';
-import HannahFirstTokenAbi from '../contracts/HannahFirstTokenAbi.json';
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
 import { parseUnits } from 'ethers/lib/utils';
+import HannahFirstTokenAbi from '../contracts/HannahFirstTokenAbi.json';
+
+import useBalance from '../hooks/useBalance';
+import useInput from '../hooks/useInput';
 
 import GivenDataForm from './common/GivenDataForm';
 import InputForm from './common/InputForm';
 
 function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
   const { ethereum } = window;
-  const { network, networkList, handleNetwork } = useNetworks();
-  const { coinVal, handleCoinVal, resetCoinVal } = useCoinInput();
-  const {
-    coinVal: tokenBal,
-    handleCoinVal: handleTokenBal,
-    resetCoinVal: resetTokenBal,
-  } = useCoinInput();
-  const { recipient, handleRecipient, resetRecipient } = useAddressInput();
-  const [errMsg, setErrMsg] = useState('');
-  const [signer, setSigner] = useState(null);
   const [provider, setProvider] = useState(null);
-  const [balance, setBalance] = useState('');
-  const [tokenBalance, setTokenBalace] = useState('');
   const [contract, setContract] = useState(null);
-  const [symbol, setSymbol] = useState('');
   const [isClickedBtn, setIsClickedBtn] = useState(false);
 
-  const checkChainId = hexChainId => {
-    // 0x3으로 보내야하는데 체인아이디를 hexString으로 바꾸면 0x03으로나와 거치는 단계
-    const regex = /^0x0/g;
-    return hexChainId.replace(regex, '0x');
-  };
-
-  useEffect(() => {
-    async function switchChainId() {
-      const hexChainID = ethers.utils.hexlify(network.id);
-      try {
-        const resSwitchChain = await ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: checkChainId(hexChainID) }],
-        });
-        if (provider) {
-          const bigNumBalance = await provider.getBalance(metaMaskAddr);
-          const balance = ethers.utils.formatUnits(bigNumBalance);
-          setBalance(balance);
-        }
-      } catch (error) {
-        if (error.code === 4902) {
-          console.error('This network is not found in your network!');
-          // 다른 이유로 네트워크를 변경하지 못했을 때 처리
-        } else {
-          console.error(error);
-        }
-      }
-    }
-    if (metaMaskAddr) switchChainId();
-  }, [network]);
-
-  useEffect(() => {
-    if (metaMaskAddr || isClickedBtn) {
-      getTokenBalance();
-    }
-    setIsClickedBtn(false);
-  }, [isClickedBtn, metaMaskAddr]);
-
-  const getTokenBalance = async () => {
-    const tokenBalance = await contract.balanceOf(metaMaskAddr);
-    setTokenBalace(ethers.utils.formatUnits(tokenBalance));
-    setSymbol(await contract.symbol());
-  };
+  const { input, handleInput, changeInput, resetInput } = useInput();
+  const { balance, getToken, symbol } = useBalance(contract);
 
   const connetingMetaMask = async () => {
     if (ethereum) {
@@ -78,52 +24,43 @@ function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const providerAcct = await provider.send('eth_requestAccounts');
-
-        const bigNumBalance = await provider.getBalance(providerAcct[0]);
-        const balance = ethers.utils.formatUnits(bigNumBalance);
-
         const contractAddr = '0xA66D992f5689D12BF41EC3a6b18445a87AfB9Fd0';
         const contract = new ethers.Contract(contractAddr, HannahFirstTokenAbi, signer);
 
         setProvider(provider);
         setMetaMaskAddr(providerAcct[0]);
-        setSigner(signer);
-        setBalance(balance);
         setContract(contract);
       } catch (err) {
-        setErrMsg(err.massage);
+        alert(err.massage);
       }
     } else {
-      setErrMsg('Install MetaMask');
+      alert('Install MetaMask');
     }
-  };
-
-  const createTx = async recipient => {
-    const currGasPrice = await provider.getGasPrice();
-    const gasPrice = ethers.utils.hexlify(parseInt(currGasPrice));
-    const value = ethers.utils.parseEther(coinVal);
-    const tx = { to: recipient, value };
-    return tx;
   };
 
   const getPkeyAddr = () => {
     if (!sendAddr) alert('개인키를 생성해주세요!');
-    else handleRecipient(sendAddr);
+    else changeInput('to', sendAddr);
   };
 
   const sendToken = async () => {
     // 따로 트랜젝션 생성하지 않고, Transfer함수를 사용한다.
     // transfer 함수 내에서 transaction을 만들어 보내주는 기능을 하는듯.
-    const tx = await contract.transfer(sendAddr, parseUnits(tokenBal));
+    const tx = await contract.transfer(sendAddr, parseUnits(input.tokenVal));
     const resTx = await provider.waitForTransaction(tx.hash);
     if (resTx) {
       setIsClickedBtn(true);
-      resetCoinVal();
-      resetTokenBal();
-      resetRecipient();
+      resetInput();
       alert('Send finished!');
     }
   };
+
+  useEffect(() => {
+    if (metaMaskAddr || isClickedBtn) {
+      getToken(metaMaskAddr);
+    }
+    setIsClickedBtn(false);
+  }, [isClickedBtn, metaMaskAddr, getToken]);
 
   return (
     <section className="container__wallet">
@@ -131,12 +68,11 @@ function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
         Meta Mask 연결
       </button>
       <GivenDataForm label={'MetaMask Address'} value={metaMaskAddr} />
-      <GivenDataForm label={'Balance'} value={balance} />
-      <GivenDataForm label={'Token'} value={tokenBalance} symbol={symbol} />
-      <InputForm label={'To'} onChange={handleRecipient}>
+      <GivenDataForm label={'Token'} value={balance} symbol={symbol} />
+      <InputForm label={'To'} name={'to'} value={input.to} onChange={handleInput}>
         <button onClick={getPkeyAddr}>PKey Addr</button>
       </InputForm>
-      <InputForm label={'Value'} onChange={handleTokenBal} />
+      <InputForm label={'Value'} name={'tokenVal'} value={input.tokenVal} onChange={handleInput} />
       <button onClick={sendToken}>Send</button>
     </section>
   );
