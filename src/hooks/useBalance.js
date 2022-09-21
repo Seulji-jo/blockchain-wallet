792ced91217d8dfb3ccc807752dd355b7c0725c5
Seulji-jo <joseuling@gmail.com>
2022-09-21 16:42:04 +0900

    [feat] balance커스텀 훅에 심볼기능 추가

--- src/hooks/useBalance.js
+++ src/hooks/useBalance.js
@@ -1,19 +1,41 @@
 import { utils } from 'ethers';
+import { useEffect } from 'react';
 import { useCallback, useState } from 'react';
 
-function useBalance(provider) {
+function useBalance(provOrContr = {}) {
   const [balance, setBalance] = useState('');
+  const [symbol, setSymbol] = useState('');
 
   const getBalance = useCallback(
     async address => {
+      const provider = provOrContr;
       const bigNumBalance = await provider.getBalance(address);
       const balance = utils.formatUnits(bigNumBalance);
       setBalance(balance);
     },
-    [provider]
+    [provOrContr]
   );
 
-  return { balance, getBalance };
+  const getToken = useCallback(
+    async address => {
+      const contract = provOrContr;
+      const tokenBalance = await contract.balanceOf(address);
+      setBalance(utils.formatUnits(tokenBalance));
+    },
+    [provOrContr]
+  );
+
+  useEffect(() => {
+    async function changeSymbol() {
+      if (provOrContr) {
+        if ('symbol' in provOrContr) setSymbol(await provOrContr?.symbol());
+        else setSymbol('ETH');
+      }
+    }
+    changeSymbol();
+  }, [provOrContr]);
+
+  return { balance, getBalance, getToken, symbol };
 }
 
 export default useBalance;
