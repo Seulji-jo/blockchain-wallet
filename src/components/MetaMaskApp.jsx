ec6a094f1e938b4f3bfc1c6c94723bf0283ddcef
Seulji-jo <joseuling@gmail.com>
2022-09-21 14:51:23 +0900

    [edit] 파일 정리

--- src/components/MetaMaskApp.jsx
+++ src/components/MetaMaskApp.jsx
@@ -1,74 +1,21 @@
-import { BigNumber, ethers } from 'ethers';
-import React, { useCallback, useEffect, useState } from 'react';
-import useAddressInput from '../hooks/useAddressInput';
-import useCoinInput from '../hooks/useCoinInput';
-import useNetworks from '../hooks/useNetworks';
-
+import { providers, utils } from 'ethers';
+import React, { useEffect, useState } from 'react';
 import GivenDataForm from './common/GivenDataForm';
 import InputForm from './common/InputForm';
 
+import useNetworks from '../hooks/useNetworks';
+import useBalance from '../hooks/useBalance';
+import useInput from '../hooks/useInput';
+
 function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
   const { ethereum } = window;
-  const { network, setNetwork, networkList, handleNetwork } = useNetworks();
-  const { coinVal, handleCoinVal, resetCoinVal } = useCoinInput();
-  const {
-    coinVal: tokenBal,
-    handleCoinVal: handleTokenBal,
-    resetCoinVal: resetTokenBal,
-  } = useCoinInput();
-  const { recipient, handleRecipient, resetRecipient } = useAddressInput();
-  const [errMsg, setErrMsg] = useState('');
   const [signer, setSigner] = useState(null);
   const [provider, setProvider] = useState(null);
-  const [balance, setBalance] = useState('');
   const [isClickedBtn, setIsClickedBtn] = useState(false);
 
-  const checkChainId = hexChainId => {
-    // 0x3으로 보내야하는데 체인아이디를 hexString으로 바꾸면 0x03으로나와 거치는 단계
-    const regex = /^0x0/g;
-    return hexChainId.replace(regex, '0x');
-  };
-
-  useEffect(() => {
-    async function switchChainId() {
-      if (metaMaskAddr) {
-        const hexChainID = ethers.utils.hexlify(network.id);
-        try {
-          const resSwitchChain = await ethereum.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: checkChainId(hexChainID) }],
-          });
-          if (provider) {
-            const bigNumBalance = await provider.getBalance(metaMaskAddr);
-            const balance = ethers.utils.formatUnits(bigNumBalance);
-            setBalance(balance);
-          }
-        } catch (error) {
-          if (error.code === 4902) {
-            console.error('This network is not found in your network!');
-            // 다른 이유로 네트워크를 변경하지 못했을 때 처리
-          } else {
-            console.error(error);
-          }
-        }
-      }
-    }
-    if (metaMaskAddr) switchChainId();
-  }, [network]);
-
-  useEffect(() => {
-    if (provider || isClickedBtn) {
-      getBalance();
-    }
-    setIsClickedBtn(false);
-  }, [isClickedBtn, provider]);
-
-  const getBalance = async () => {
-    const providerAcct = await provider.send('eth_requestAccounts');
-    const bigNumBalance = await provider.getBalance(providerAcct[0]);
-    const balance = ethers.utils.formatUnits(bigNumBalance);
-    setBalance(balance);
-  };
+  const { network, networkList, handleNetwork, switchMetaMaskChain } = useNetworks();
+  const { input, handleInput, changeInput, resetInput } = useInput();
+  const { balance, getBalance } = useBalance(provider);
 
   const handleAccountsChanged = async accounts => {
     try {
@@ -77,64 +24,61 @@ function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
       console.log(error);
     }
   };
-
-  useEffect(() => {
-    if (metaMaskAddr) {
-      // ethereum.on('chainChanged', handleChainChanged);
-      ethereum.on('accountsChanged', handleAccountsChanged);
+  const handleChainChanged = async chainId => {
+    try {
+      const network = networkList.filter(li => li.id === parseInt(chainId, 16));
+      handleNetwork(network[0].network);
+    } catch (error) {
+      console.log(error);
     }
-    return () => {
-      // ethereum.removeListener('chainChanged', handleChainChanged);
-      ethereum.removeListener('accountsChanged', handleAccountsChanged);
-    };
-  }, [ethereum]);
+  };
 
   const connetingMetaMask = async () => {
     if (ethereum) {
       try {
-        const provider = new ethers.providers.Web3Provider(ethereum, 'any');
+        const provider = new providers.Web3Provider(ethereum, 'any');
         const signer = provider.getSigner();
         const providerAcct = await provider.send('eth_requestAccounts');
+        const addr = await signer.getAddress();
         const currChain = await ethereum.request({ method: 'eth_chainId' });
         const network = networkList.filter(li => li.id === parseInt(currChain, 16));
 
         setProvider(provider);
         setMetaMaskAddr(providerAcct[0]);
         setSigner(signer);
-        setNetwork(network[0]);
+        handleNetwork(network[0].network);
       } catch (err) {
-        setErrMsg(err.massage);
+        alert(err.massage);
       }
     } else {
-      setErrMsg('Install MetaMask');
+      alert('Install MetaMask');
     }
-  };
-
-  const createTx = async recipient => {
-    const currGasPrice = await provider.getGasPrice();
-    const gasPrice = ethers.utils.hexlify(parseInt(currGasPrice));
-    const value = ethers.utils.parseEther(coinVal);
-    const tx = { to: recipient, value };
-    return tx;
   };
 
   const getPkeyAddr = () => {
     if (!sendAddr) alert('개인키를 생성해주세요!');
-    else handleRecipient(sendAddr);
+    else changeInput('to', sendAddr);
+  };
+
+  const createTx = async () => {
+    const currGasPrice = await provider.getGasPrice();
+    const gasPrice = utils.hexlify(parseInt(currGasPrice));
+    const value = utils.parseEther(input.value);
+    const tx = { to: input.to, value };
+    return tx;
   };
 
   const sendTransaction = async () => {
-    if (!coinVal) {
+    if (!input.value) {
       alert('Value를 입력해주세요');
     } else {
-      const tx = await createTx(recipient);
+      const tx = await createTx();
       try {
         const sendTx = await signer.sendTransaction(tx);
         const resTx = await provider.waitForTransaction(sendTx.hash);
         if (resTx) {
           setIsClickedBtn(true);
-          resetCoinVal();
-          resetRecipient();
+          resetInput();
           alert('Send finished!');
         }
       } catch (error) {
@@ -144,6 +88,37 @@ function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
     }
   };
 
+  useEffect(() => {
+    const chageProvider = async () => {
+      if (metaMaskAddr) {
+        const isChangedChain = await switchMetaMaskChain();
+        if (isChangedChain) {
+          const provider = new providers.Web3Provider(ethereum, 'any');
+          setProvider(provider);
+        }
+      }
+    };
+    chageProvider();
+  }, [ethereum, metaMaskAddr, network, switchMetaMaskChain]);
+
+  useEffect(() => {
+    if (provider || isClickedBtn) {
+      getBalance(metaMaskAddr);
+    }
+    setIsClickedBtn(false);
+  }, [isClickedBtn, provider, getBalance, metaMaskAddr]);
+
+  useEffect(() => {
+    if (ethereum) {
+      ethereum.on('chainChanged', handleChainChanged);
+      ethereum.on('accountsChanged', handleAccountsChanged);
+    }
+    return () => {
+      ethereum.removeListener('chainChanged', handleChainChanged);
+      ethereum.removeListener('accountsChanged', handleAccountsChanged);
+    };
+  });
+
   return (
     <section className="container__wallet">
       <button onClick={connetingMetaMask} disabled={metaMaskAddr}>
@@ -151,11 +126,15 @@ function MetaMaskApp({ sendAddr, metaMaskAddr, setMetaMaskAddr }) {
       </button>
       <GivenDataForm label={'MetaMask Address'} value={metaMaskAddr} />
       <GivenDataForm label={'Balance'} value={balance} />
-      <InputForm label={'To'} onChange={handleRecipient}>
+      <InputForm label={'To'} name={'to'} value={input.to} onChange={handleInput}>
         <button onClick={getPkeyAddr}>PKey Addr</button>
       </InputForm>
-      <InputForm label={'Value'} onChange={handleCoinVal}>
-        <select name="networks" id="networks" value={network.network} onChange={handleNetwork}>
+      <InputForm label={'Value'} name={'value'} value={input.value} onChange={handleInput}>
+        <select
+          name="networks"
+          // id="networks"
+          value={network?.network}
+          onChange={e => handleNetwork(e?.target?.value)}>
           {networkList.map(network => (
             <option key={network.id} value={network.network}>
               {network.name}
