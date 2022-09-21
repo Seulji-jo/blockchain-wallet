065c5e49540b3bf79ae2b93d3c57322f20c6e049
Seulji-jo <joseuling@gmail.com>
2022-09-21 15:45:39 +0900

    [edit] ERC20 page 수정(해당 토큰 정보 추가 및 스타일 수정)

--- src/pages/Erc20.jsx
+++ src/pages/Erc20.jsx
@@ -7,13 +7,28 @@ function Erc20() {
   const [newAddr, setNewAddr] = useState('');
 
   return (
-    <div className="row">
-      <MetaMaskErc20
-        sendAddr={newAddr}
-        metaMaskAddr={metaMaskAddr}
-        setMetaMaskAddr={setMetaMaskAddr}
-      />
-      <NewPKeyErc20 sendAddr={metaMaskAddr} newAddr={newAddr} setNewAddr={setNewAddr} />
+    <div className="column">
+      <h2>HannahFirstToken (HFT)</h2>
+      <ul>
+        <li>Contract Address: 0xA66D992f5689D12BF41EC3a6b18445a87AfB9Fd0</li>
+        <li>Network: Ropsten</li>
+        <li>
+          <a
+            href="https://ropsten.etherscan.io/address/0xA66D992f5689D12BF41EC3a6b18445a87AfB9Fd0"
+            target="_blank"
+            rel="noreferrer">
+            Etherscan에서 확인하기
+          </a>
+        </li>
+      </ul>
+      <div className="row">
+        <MetaMaskErc20
+          sendAddr={newAddr}
+          metaMaskAddr={metaMaskAddr}
+          setMetaMaskAddr={setMetaMaskAddr}
+        />
+        <NewPKeyErc20 sendAddr={metaMaskAddr} newAddr={newAddr} setNewAddr={setNewAddr} />
+      </div>
     </div>
   );
 }
