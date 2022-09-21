4e4d7ab6986dbd256b819121d6be233e491c370c
Seulji-jo <joseuling@gmail.com>
2022-09-21 17:11:55 +0900

    [edit] SendNft 파일 정리

--- src/components/SendNft.jsx
+++ src/components/SendNft.jsx
@@ -1,27 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { ethers } from 'ethers';
 import HannahNftAbi from '../contracts/HannahNftAbi.json';
-import useAddressInput from '../hooks/useAddressInput';
-import useCoinInput from '../hooks/useCoinInput';
+import useInput from '../hooks/useInput';
 
 import InputForm from './common/InputForm';
 
 function SendNft() {
-  const {
-    recipient: fromAddr,
-    handleRecipient: handleFromAddr,
-    resetRecipient: resetFromAddr,
-  } = useAddressInput();
-  const {
-    recipient: toAddr,
-    handleRecipient: handleToAddr,
-    resetRecipient: resetToAddr,
-  } = useAddressInput();
-  const {
-    coinVal: tokenId,
-    handleCoinVal: handleTokenId,
-    resetCoinVal: resetTokenId,
-  } = useCoinInput();
+  const { input, handleInput, resetInput } = useInput();
 
   const getSigner = () => {
     const pKey = '0x15b3eed746443d676410608e93459026a560672cd2032a719cfa2137caa06a80';
@@ -31,28 +16,31 @@ function SendNft() {
   };
 
   const handleTransferNft = async () => {
-    console.log('handleTransferNft');
     const contractAddr = '0x7528D0211c5926EbFddFE9FBCafFDdC8F6adC5f8';
     const providerSigner = getSigner();
     const contract = new ethers.Contract(contractAddr, HannahNftAbi, providerSigner);
-    // const uri = `ipfs://${tokenUri}`;
-    // ipfs를 붙여 민팅을 하게 되면 메타마스크에서 이미지가 나타나지 않는다.
+
+    const { from, to, tokenId } = input;
+
     const transferringNft = await contract['safeTransferFrom(address,address,uint256)'](
-      fromAddr,
-      toAddr,
+      from,
+      to,
       tokenId
     );
-    console.log(transferringNft);
-    if (transferringNft) alert('NFT 전송 성공');
+
+    if (transferringNft) {
+      resetInput();
+      alert('NFT 전송 성공');
+    }
   };
 
   return (
     <section className="container__wallet">
       <h4 className="title">Send NFT</h4>
-      <InputForm label={'From'} onChange={handleFromAddr} />
-      <InputForm label={'To'} onChange={handleToAddr} />
-      <InputForm label={'Token Id'} onChange={handleTokenId} />
-      <button onClick={handleTransferNft} disabled={!fromAddr || !toAddr || !tokenId}>
+      <InputForm label={'From'} name={'from'} value={input.from} onChange={handleInput} />
+      <InputForm label={'To'} name={'to'} value={input.to} onChange={handleInput} />
+      <InputForm label={'Token Id'} name={'tokenId'} value={input.tokenId} onChange={handleInput} />
+      <button onClick={handleTransferNft} disabled={!input.from || !input.to || !input.tokenId}>
         send
       </button>
     </section>
