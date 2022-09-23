31c6ab8f177ac04cc51342a9922afb040c881169
Seulji-jo <joseuling@gmail.com>
2022-09-23 17:25:49 +0900

    [delete] 필요없는 파일 삭제

--- src/hooks/useSubmitBtn.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-function useSubmitBtn() {
-  const [isSendComplete, setIsSendComplete] = useState(false);
-
-  useState(() => {}, [isSendComplete]);
-}
-
-export default useSubmitBtn;
